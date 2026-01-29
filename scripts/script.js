1c516314a77f8c4b8aaf54a06d51f94b2aff03c1
Marco <phanvutuananh75@gmail.com>
2026-01-28 23:31:31 -0800

    update hrefs + iframes for sources

--- scripts/script.js
+++ scripts/script.js
@@ -40,6 +40,10 @@ const resources = [
         emotions: ["challenging", "reflective"],
         format: "video",
         thumbnail_url: "",
+        href_url: "https://youtu.be/1s2yPnYgQOM?si=rSmC9tujtXAi7Hgd",
+        embeddable: true,
+        embedded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/1s2yPnYgQOM?si=TIaFl1O5lp0X_r6c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
         id: "13th",
@@ -47,13 +51,19 @@ const resources = [
         emotions: ["challenging"],
         format: "video",
         thumbnail_url: "",
+        href_url: "https://youtu.be/krfcq5pF8u8?si=qj_Z8njS8-SxUEKD",
+        embeddable: true,
+        embedded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/krfcq5pF8u8?si=yj8ouV9a89PxIE1I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
         id: "acd",
         title: "A Class Divided",
         emotions: ["reflective", "empathetic"],
         format: "video",
-        thumbnail_url: "",
+        thumbnail_url: "./thumbnails/acd.png",
+        href_url:
+            "https://www.pbs.org/wgbh/frontline/documentary/class-divided/?",
     },
     {
         id: "obl",
@@ -61,6 +71,10 @@ const resources = [
         emotions: ["reflective", "empathetic"],
         format: "interview",
         thumbnail_url: "",
+        href_url: "https://youtu.be/uqK2_w4k5hA?si=-9ipDDUb2X2D_Zun",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/uqK2_w4k5hA?si=-TdDqwkeQb5krhN-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
         id: "ctc",
@@ -68,27 +82,41 @@ const resources = [
         emotions: ["challenging"],
         format: "video",
         thumbnail_url: "",
+        href_url: "https://youtu.be/GTvU7uUgjUI?si=zpzVctz-hwNs08mq",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/GTvU7uUgjUI?si=zpzVctz-hwNs08mq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
-        id: "agape",
-        title: "King’s Message of Love and Agape",
+        id: "kinglove",
+        title: "King’s Message of Love",
         emotions: ["uplifting", "empathetic"],
         format: "talk",
-        thumbnail_url: "",
+        thumbnail_url: "./thumbnails/kinglove.png",
+        href_url:
+            "https://theconversation.com/why-a-fractured-nation-needs-to-remember-kings-message-of-love-68643",
+        embbedable: false,
+        embbeded_iframe: "",
     },
     {
         id: "sm",
         title: "Social Mindfulness",
         emotions: ["reflective", "calming"],
         format: "article",
-        thumbnail_url: "",
+        thumbnail_url: "./thumbnails/sm.png",
+        href_url: "https://www.socialmindfulness.nl/",
+        embbedable: false,
+        embbeded_iframe: "",
     },
     {
         id: "med",
-        title: "Meditating Lessons",
+        title: "Headspace Focus Breathing/Meditating Lessons",
         emotions: ["calming"],
         format: "exercise",
-        thumbnail_url: "",
+        thumbnail_url: "./thumbnails/headspace.png",
+        href_url: "https://www.headspace.com/",
+        embbedable: false,
+        embbeded_iframe: "",
     },
     {
         id: "conv",
@@ -96,6 +124,10 @@ const resources = [
         emotions: ["practical", "uplifting"],
         format: "talk",
         thumbnail_url: "",
+        href_url: "https://youtu.be/R1vskiVDwl4?si=J1hz7AxA70QG4d6M",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/R1vskiVDwl4?si=Zq1QXaFbcalp31PT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
         id: "coach",
@@ -103,6 +135,10 @@ const resources = [
         emotions: ["motivational", "calming"],
         format: "talk",
         thumbnail_url: "",
+        href_url: "https://youtu.be/q7a5TIzOmeQ?si=B5B1GnpGxd-lbzY0",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/q7a5TIzOmeQ?si=-EkenCdDXKSjaSs_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
         id: "one",
@@ -110,13 +146,21 @@ const resources = [
         emotions: ["motivational"],
         format: "talk",
         thumbnail_url: "",
+        href_url: "https://youtu.be/kVScc1CRxgs?si=ISmhyFXsvYuKQAFv",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/kVScc1CRxgs?si=UyvfIJjYyYHzTCT_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
         id: "rbk",
         title: "How to Raise Brilliant Kids",
         emotions: ["uplifting", "empathetic"],
         format: "article",
-        thumbnail_url: "",
+        thumbnail_url: "./thumbnails/rbk.png",
+        href_url:
+            "https://www.npr.org/sections/ed/2018/07/23/627265706/raising-brilliant-kids-with-research-to-back-you-up?utm_medium=RSS&utm_campaign=npred",
+        embbedable: false,
+        embbeded_iframe: "",
     },
     {
         id: "goodlife",
@@ -124,13 +168,21 @@ const resources = [
         emotions: ["reflective", "uplifting"],
         format: "talk",
         thumbnail_url: "",
+        href_url: "https://youtu.be/8KkKuTCFvzI?si=VkCISpjZRM3XeL5L",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/8KkKuTCFvzI?si=VkCISpjZRM3XeL5L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
     {
-        id: "love",
-        title: "An Experiment in Love",
-        emotions: ["reflective", "empathetic"],
+        id: "kingagape",
+        title: "Six Pillars of Nonviolent Resistance and ‘Agape’",
+        emotions: ["motivational", "empathetic"],
         format: "article",
-        thumbnail_url: "",
+        thumbnail_url: "./thumbnails/kingagape.png",
+        href_url:
+            "https://www.themarginalian.org/2015/07/01/martin-luther-king-jr-an-experiment-in-love/",
+        embbedable: false,
+        embbeded_iframe: "",
     },
     {
         id: "brain",
@@ -138,6 +190,10 @@ const resources = [
         emotions: ["mind-opening", "reflective"],
         format: "talk",
         thumbnail_url: "",
+        href_url: "https://youtu.be/hQaN5w3YwtM?si=Ry00W7nhuAgen3PH",
+        embbedable: true,
+        embbeded_iframe:
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/hQaN5w3YwtM?si=Ry00W7nhuAgen3PH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
     },
 ];
 
